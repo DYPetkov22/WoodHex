2b5a931607ce7d71fc1170f4df4c55d3aaf8c041
LRRashkov22 <LRRashkov22@codingburgas.bg>
2023-11-12 08:32:41 -0800

    Add haburger nav-bar menu

---
+++ main.js
@@ -0,0 +1,6 @@
+const toggle = () => { 
+    let icon = document.getElementById('burger')
+    let dropdown = document.getElementById('navbarBasicExample')
+    icon.classList.toggle('is-active')
+    dropdown.classList.toggle('is-active')
+} 
