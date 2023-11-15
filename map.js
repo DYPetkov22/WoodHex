6bcd79c1fc1eb9a36a21e252a2717d0a0ac38a6e
Anton Teodorov Kabakov <ATKabakov22@codingburgas.bg>
2023-11-15 20:12:41 +0200

    Make the map full screen

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 let southWest = L.latLng(42.5, 25),northEast = L.latLng(43, 26);
 let bounds = L.latLngBounds(southWest, northEast);
-let centerCoordinates = [42.65, 27.6];
+let centerCoordinates = [42.65, 28.2];
 let map = L.map('leaflet-map', 
 {
     center: centerCoordinates,
@@ -21,7 +21,7 @@ const g = svg.append("g").attr("class", "leaflet-zoom-hide");
 const rows = 15;
 const cols = 26;
 
-let hexagonRadius = 35;
+let hexagonRadius = 45;
 let hexagonWidth = hexagonRadius * Math.sqrt(3);
 let hexagonHeight = hexagonRadius * 2;
 let hoveredColor = "";
