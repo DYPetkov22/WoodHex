f9fed897de92c4b3e98612dd5e53798822572ac8
Anton Teodorov Kabakov <ATKabakov22@codingburgas.bg>
2023-11-15 19:35:42 +0200

    Add the main values of the map

--- map.js
+++ map.js
@@ -13,3 +13,15 @@ L.tileLayer('https://api.maptiler.com/maps/basic-v2-dark/256/{z}/{x}/{y}.png?key
     minZoom: 8,
     maxZoom: 8,
 }).addTo(map);
+
+const randomImagePaths = ["green.svg", "red.svg"];
+const alternativeImagePaths = ["green-glow.svg", "red-glow.svg"];
+const svg = d3.select(map.getPanes().overlayPane).append("svg");
+const g = svg.append("g").attr("class", "leaflet-zoom-hide");
+const rows = 15;
+const cols = 26;
+
+let hexagonRadius = 35;
+let hexagonWidth = hexagonRadius * Math.sqrt(3);
+let hexagonHeight = hexagonRadius * 2;
+let hoveredColor = "";
