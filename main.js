f5d7174557bf072ada0b4b8a8c96e16f071a2485
ATKabakov22 <atkabakov22@codingburgas.bg>
2023-12-05 09:45:41 +0200

    Disable scrowing on the homepage

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-const toggle = () => { 
+const toggle = () => {
     let icon = document.getElementById('burger')
     let dropdown = document.getElementById('navbarBasicExample')
     icon.classList.toggle('is-active')
     dropdown.classList.toggle('is-active')
-} 
+}
+
+document.body.classList.add("stop-scrolling");
+
+function enableScroll() {
+    document.body.classList.remove("stop-scrolling");
+}
